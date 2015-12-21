563b82d7b5bc7cb1c1fbb75b1781aab3eeb0f73b
Mattéo Delabre <contact@matteodelabre.me>
2015-12-21 22:32:04 +0100

    :bulb: Add `create` method

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ exports.import = (node) => wrapNode(node);
  * @param {HTMLDocument} doc Document to import
  * @return {Object} A hash with doctype, body, head, html props
  */
-exports.html = (node) => ({
-    body: wrapNode(node.body),
-    head: wrapNode(node.head),
-    html: wrapNode(node.documentElement),
-    doctype: wrapNode(node.doctype)
+exports.html = doc => ({
+    create: name => doc.createElement(name),
+    body: wrapNode(doc.body),
+    head: wrapNode(doc.head),
+    html: wrapNode(doc.documentElement),
+    doctype: wrapNode(doc.doctype)
 });
